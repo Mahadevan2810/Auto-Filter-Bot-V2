00b9e878675f7a2802d05c4359c6c4d96781799d
Iamnoob2255 <97869020+Iamnoob2255@users.noreply.github.com>
2022-01-17 11:55:57 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -9,7 +9,7 @@ class Translation(object):
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
+©️Team Malayalam Movie Factory™️..
 @MMF_chat
 </i>"""    
     
