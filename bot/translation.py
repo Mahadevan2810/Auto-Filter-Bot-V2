ec61d1971e04b48a72c839d766238ad58fe755aa
Iamnoob2255 <97869020+Iamnoob2255@users.noreply.github.com>
2022-01-17 11:19:41 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@LXRBOTSGROUP
+@MMF_chat
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@LXRBOTSGROUP
+@Manglasserryy_neelakandan
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/@malayalammoviesmmms">LXR-YOUTUBER</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/@manglasseryy_neelakandan">manglasseryy_neelakandan</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/LXR-YOUTUBER-AMAL/Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Iamnoob2255/Auto-Filter-Bot-V2">Click Me</a></i>
 """
